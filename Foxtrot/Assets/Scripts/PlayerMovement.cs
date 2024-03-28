fc41c88ec9c5fc1993a5f9cc2cffc1c9826a0660
Jack Justus <58899065+jackjustus@users.noreply.github.com>
2024-03-27 17:34:16 -0700

    movement and camera updates

---
+++ Foxtrot/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float characterSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+
+    // Runs once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * characterSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    // Runs a fixed number of times per second
+    void FixedUpdate()
+    {
+        // Move our character
+        // controller.Move(speed, crouch, jump)
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        jump = false;
+        
+
+    }
+
+
+}
