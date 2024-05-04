e8dd9c925b2dede08d0892201f58f3157cd126c8
Jack Justus <91996103+Jack-Justus@users.noreply.github.com>
2024-05-03 18:21:40 -0700

    clip Masking

--- Foxtrot/Assets/Scripts/Utility/clipMasking.cs
+++ Foxtrot/Assets/Scripts/Utility/clipMasking.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class clipMasking : MonoBehaviour
 {
-    void Awake() {
-        
+    void Awake()
+    {
+        // Disables the sprite renderer of all children
+        SpriteRenderer[] spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        foreach (SpriteRenderer spriteRenderer in spriteRenderers)
+        {
+            spriteRenderer.enabled = false;
+        }
     }
 }
