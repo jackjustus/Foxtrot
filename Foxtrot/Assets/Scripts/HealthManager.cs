ab76fdc9b6de8fd984a5e01dacf367392cdc9a77
kjchen05 <kjchen2005@gmail.com>
2024-05-16 19:44:06 -0700

    idk

---
+++ Foxtrot/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField] private int health { get; set; }
+
+    private int MAX_HEALTH = 5;
+
+
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+    
+        
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Heal(1);
+        }
+
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            Damage(1);
+        }
+
+    }
+
+    public void Damage(int amount)
+    {
+
+        if(amount<0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative Damage");
+        } 
+        this.health -= amount;
+
+        if(health<=0)
+        {
+            Die();
+        }
+
+        
+    }
+
+    public void Heal(int amount)
+    {
+
+        if (amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative Healing");
+        }
+
+        bool wouldBeOverMaxHealth = health + amount > MAX_HEALTH;
+
+
+        if (wouldBeOverMaxHealth)
+        {
+            this.health = MAX_HEALTH;
+        }
+        else
+        {
+            this.health += amount;
+        }
+
+    }
+
+    private void Die()
+    {
+        
+        UnityEngine.Debug.Log("I am Dead");
+        Destroy(gameObject);
+    }
+}
---
+++ Foxtrot/Assets/Scripts/UpAttackArea.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpAttackArea : MonoBehaviour
+{
+    private int damage = 5;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+
+
+
+        if (collider.GetComponent<Health>() != null)
+        {
+            Health health = collider.GetComponent<Health>();
+            health.Damage(damage);
+
+
+        }
+    }
+}
