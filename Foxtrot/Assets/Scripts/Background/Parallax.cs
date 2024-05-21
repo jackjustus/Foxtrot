b5a50317d747a6640d6bdff748bcb5890339ccc2
Jack Justus <91996103+Jack-Justus@users.noreply.github.com>
2024-05-20 17:34:32 -0700

    Background cleanup & Tuning

--- Foxtrot/Assets/Scripts/Background/Parallax.cs
+++ Foxtrot/Assets/Scripts/Background/Parallax.cs
@@ -5,41 +5,58 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
 
-    private float length, startPosX;
-    private GameObject cam;
-    public float parallaxEffect, startPosY;                    // The greater the parallax coefficent, the more the background moves with the camera
+    private GameObject cam;                                         // The game camera
+    [SerializeField] bool doParallaxX, doParallaxY;                 // Enable / Disable Parallax motion
 
-    [SerializeField] bool doParallaxY;
-    public float parallaxEffectY;
+    [Header("Parallax Coefficients")]
+    [SerializeField] public float parallaxEffectX;                  // The greater the parallax coefficent, the more the background moves with the camera
+    [SerializeField] public float parallaxEffectY;                  // The greater the parallax coefficent, the more the background moves with the camera
+    
+    [Header("Parallax Bias")]
+    [SerializeField] public float startPosY;                        // The starting position of the background. Almost acts like a bias
 
-    private float distanceTraveled, parallaxDistance, parallaxDistanceY;
+    private float lengthX, startPosX, distanceTraveledX, parallaxDistanceX; // Helper Variables for the x axis
+    private float parallaxDistanceY;                                        // Helper variables for the y axis
+
 
     void Start()
     {
+        // Initialize the game camera. Used to track the camera's position
         cam = GameObject.FindGameObjectWithTag("MainCamera");
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
 
-
+        // Get the length of the background. Used to determine when to reset the background
+        lengthX = GetComponent<SpriteRenderer>().bounds.size.x;
     }
+
+
 
     void FixedUpdate()
     {
 
-        distanceTraveled = 0;                                                       // IDK What this does, but it always is 0               // distanceTraveled = cam.transform.position.x * (1 - parallaxEffect);   
-        parallaxDistance = cam.transform.position.x * parallaxEffect;               // Camera distance in x axis * parallax effect = how much the background should move
+        distanceTraveledX = 0;                                                              // IDK What this does, but it always is 0               // distanceTraveled = cam.transform.position.x * (1 - parallaxEffect);   
 
 
-        if (doParallaxY)
-            parallaxDistanceY = startPosY + (cam.transform.position.y * parallaxEffectY);         // Camera dist from start in y axis  * parallax effect = how much the background should move
+        // Calculating Parallax X
+        if (doParallaxX)
+            parallaxDistanceX = startPosX + (cam.transform.position.x * parallaxEffectX);   // How much the background should move (X axis)
         else
-            parallaxDistanceY = transform.position.y;                               // Keep the Y constant
+            parallaxDistanceX = transform.position.x;                                       // Keep the X constant
+
+        // Calculating Parallax Y
+        if (doParallaxY)
+            parallaxDistanceY = startPosY + (cam.transform.position.y * parallaxEffectY);   // How much the background should move (Y axis)
+        else
+            parallaxDistanceY = transform.position.y;                                       // Keep the Y constant
         
 
-        // Moving the background to the start pos + parallax distance
-        transform.position = new Vector3(startPosX + parallaxDistance, parallaxDistanceY, transform.position.z);
 
-        if (distanceTraveled > startPosX + length) startPosX += length;
-        else if (distanceTraveled < startPosX - length) startPosX -= length;
+        // Updating the background's position with the new parallax distance
+        transform.position = new Vector3(startPosX + parallaxDistanceX, parallaxDistanceY, transform.position.z);
+
+
+        // Resetting the background if it goes out of bounds
+        if (distanceTraveledX > startPosX + lengthX) startPosX += lengthX;
+        else if (distanceTraveledX < startPosX - lengthX) startPosX -= lengthX;
 
         // Debug
         // Debug.Log("Distance Traveled: " + distanceTraveled + " Parallax Distance: " + parallaxDistance);
