106100bb09992dd56bbf315104e1c56771a4663f
Jack Justus <91996103+Jack-Justus@users.noreply.github.com>
2024-05-22 13:19:37 -0700

    push

---
+++ Foxtrot/Assets/Scripts/Camera/CameraFollowObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowObject : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform playerTransform;
+
+    [Header("Flip Rotation")]
+    [SerializeField] private float flipYRotationTime = 0.5f;
+    private Coroutine turnCoroutine;
+    private PlayerMovement1 player;
+    private bool isFacingRight;
+
+    private void Awake() {
+        player = playerTransform.gameObject.GetComponent<PlayerMovement1>();
+
+        isFacingRight = player.IsFacingRight;
+    }
+    
+
+
+}
